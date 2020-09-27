eb305f8711eaf85a55ecc56d04e9b538fc594c02
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-27 20:33:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,13 +29,10 @@ class Song
  end 
  
  def self.genre_count
-   genre_count = Hash.new
-   
-  def self.genre_count
-    genre_count = Hash.new(0)
-    @@genres.each {|genre| genre_count[genre] += 1}
-    genre_count
-  end 
+   genre_count = Hash.new(0)
+   @@genres.each {|genre| genre_count[genre] += 1}
+   genre_count
+ end 
  
   
 end 
