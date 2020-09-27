7bf14f1711b58f85cdf410e04e5e98b6d40ea58f
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-27 21:09:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,41 @@
 class Song 
-  
-  attr_accessor :name, :artist, :genre 
-  @@count = 0 
-  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+  @@count = 0
   @@artists = []
-  
-  
-  def initialize(song_name, artist, genre) 
+  @@genres = []
+
+  def initialize(song_name,artist,genre)
     @name = song_name
     @artist = artist
-    @genre = genre 
-    @@song_count += 1
+    @genre = genre
+    @@count += 1
+    @@artists << artist
     @@genres << genre
-    @@artists << artist 
-    
-  end 
-  
-  def self.count
-    @@song_count
-  end 
-  
-  def self.genre
+  end
+
+  def self.count 
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
     @@genres.uniq
-  end 
-    
- def self.artists 
-   @@artists.uniq
- end 
- 
- def self.genre_count
-   genre_count = Hash.new(0)
-   @@genres.each {|genre| genre_count[genre] += 1}
-   genre_count
- end 
- 
- def self.artist_count
-   artist_count = Hash.new(0)
-   @@artists.each {|artist| artist_count[artist] += 1}
- end 
- 
-  
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
+
 end 
