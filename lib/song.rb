d3b47cac948b09c47bc6cbb67630aa11f96ad707
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-09-27 20:11:51 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  @@count = 0 
+  
+  
+  def initialize(song_name, artist, genre) 
+    @name = song_name
+    
+    @@song_count += 1
+  end 
+  
+  def self.count
+    @@song_count
+  end 
+  
+  def genre
+    @@
+    
+ 
+ 
+  
+end 
